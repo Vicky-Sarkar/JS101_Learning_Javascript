00ad9ef7cd7419f860484d26347e14bf12681a51
vickysarkar7 <vickyysarkar@gmail.com>
2022-10-11 09:22:07 +0530

    day1 all files add

---
+++ problem2.js
@@ -0,0 +1,9 @@
+
+var name="Vicky Sarkar"
+console.log(name)
+
+var father_name="Viswanath Sarkar"
+console.log(father_name)
+
+var mother_name="Sheema Sarkar"
+console.log(mother_name)
---
+++ problem4.js
@@ -0,0 +1,21 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+
+
+var name="Name-----------------Vicky Sarkar"
+var school="School------------Delhi Pablic School"
+var grade="Gread--------------A+"
+var section="Section----------- A Section"
+var mark="Marks P C M ------------80,95,70"
+console.log("------------Report Card------------")
+
+ console.log(name)
+
+ console.log(school)
+
+ console.log(grade)
+
+ console.log(section)
+
+ console.log(mark)
